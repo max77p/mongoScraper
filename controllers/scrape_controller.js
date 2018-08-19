31356cd580da75e8cf8f5bb73b33cc62c04437dd
max77p <shanindrakumar@gmail.com>
2018-08-18 22:23:18 -0400

    sent to db and added route to get from db as test

--- controllers/scrape_controller.js
+++ controllers/scrape_controller.js
@@ -1,26 +1,59 @@
-var express=require("express");
-var router=express.Router();
+var express = require("express");
+var router = express.Router();
 var cheerio = require("cheerio");
 var request = require("request");
+var db = require("../models/db.js");
 
-router.get("/scrape",function(req,res){
+router.get("/scrape", function (req, res) {
     request("https://old.reddit.com/r/javascript/hot/", function (err, res, html) {
         var $ = cheerio.load(html);
         var results = [];
-    
+
         $("p.title").each(function (i, element) {
-                var title=$(element).text();
-                var link =$(element).children().attr("href");
-    
-                results.push({
-                    title:title,
-                    link:link
-                })
+            var title = $(element).text();
+            var link = $(element).children().attr("href");
+
+            results.push({
+                title: title,
+                link: link
+            })
+
+            // If this found element had both a title and a link
+            if (title && link) {
+                // Insert the data in the scrapedData db
+                db.scrapedData.insert({
+                        title: title,
+                        link: link
+                    },
+                    function (err, inserted) {
+                        if (err) {
+                            // Log the error if one is encountered during the query
+                            console.log(err);
+                        } else {
+                            // Otherwise, log the inserted data
+                            console.log(inserted);
+                        }
+                    });
+            }
         });
         console.log(results);
-    
     })
+    res.send("scrape complete");
 });
 
 
-module.exports=router;
+router.get("/all", function (req, res) {
+    // Find all results from the scrapedData collection in the db
+    db.scrapedData.find({}, function (error, found) {
+        // Throw any errors to the console
+        if (error) {
+            console.log(error);
+        }
+        // If there are no errors, send the data to the browser as json
+        else {
+            res.json(found);
+        }
+    });
+});
+
+module.exports = router;
---
+++ models/db.js
@@ -0,0 +1,13 @@
+
+var mongojs=require("mongojs");
+//database configuration
+var databaseUrl="scraper";
+var collections=["scrapedData"];
+
+//hook mongojs config to the db variable
+var db=mongojs(databaseUrl,collections);
+db.on("error",function(error){
+    console.log("Database Error:", error);
+})
+
+module.exports = db;
